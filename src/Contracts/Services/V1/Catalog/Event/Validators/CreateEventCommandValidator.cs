59059e7ab97f65004245d0e8d2cfc8710cc3e857
mnnam1302 <mnnam1302>
2024-05-28 20:25:03 +0700

    feat-(catalog): implement API create Event

---
+++ src/Contracts/Services/V1/Catalog/Event/Validators/CreateEventCommandValidator.cs
@@ -0,0 +1,59 @@
+﻿using Contracts.Enumerations;
+using FluentValidation;
+
+namespace Contracts.Services.V1.Catalog.Event.Validators;
+
+public class CreateEventCommandValidator : AbstractValidator<Command.CreateEventCommand>
+{
+    public CreateEventCommandValidator()
+    {
+        RuleFor(x => x.Name)
+            .NotEmpty();
+
+        // StartedDateOnUtc must be greater than or equal to the current date and time.
+        RuleFor(x => x.StartedDateOnUtc)
+            .NotEmpty()
+            .GreaterThanOrEqualTo(DateTimeOffset.UtcNow);
+
+        // EndedDateOnUtc must be greater than or equal to StartedDateOnUtc.
+        RuleFor(x => x.EndedDateOnUtc)
+            .NotEmpty()
+            .GreaterThanOrEqualTo(x => x.StartedDateOnUtc);
+
+        RuleFor(x => x.Capacity)
+            .GreaterThan(0);
+
+        RuleFor(x => x.OrganizationId)
+            .NotEmpty();
+
+
+        // Rule for EventType must be Online and MeetUrl must not be null or empty
+        // or EventType must be Offline and Adrress, District, City, and Country must not be null or empty.
+        // I'd like to MeetUrl í https./...
+        RuleFor(x => x.EventType)
+            .NotEmpty()
+            .Must(x => x.Equals(EventType.Online) || x.Equals(EventType.Offline));
+
+        When(x => x.Equals(EventType.Online), () =>
+        {
+            RuleFor(x => x.MeetUrl)
+                .NotEmpty()
+                .Matches(@"^(http|https)://[a-zA-Z0-9\-\.]+\.[a-zA-Z]{2,3}(/\S*)?$");
+        });
+
+        When(x => x.Equals(EventType.Offline), () =>
+        {
+            RuleFor(x => x.Adrress)
+                .NotEmpty();
+
+            RuleFor(x => x.District)
+                .NotEmpty();
+
+            RuleFor(x => x.City)
+                .NotEmpty();
+
+            RuleFor(x => x.Country)
+                .NotEmpty();
+        });
+    }
+}
---
+++ src/Services/Catalog/Application/UseCases/V1/Commands/Event/CreateEventCommandHandler.cs
@@ -0,0 +1,65 @@
+﻿using Contracts.Abstractions.Message;
+using Contracts.Abstractions.Shared;
+using Contracts.Services.V1.Catalog.Event;
+using Domain.Abstractions;
+using Domain.Abstractions.Repositories;
+using Domain.Exceptions;
+
+namespace Application.UseCases.V1.Commands.Event;
+
+public class CreateEventCommandHandler : ICommandHandler<Command.CreateEventCommand>
+{
+    private readonly IRepositoryBase<Domain.Entities.Event, Guid> _eventRepository;
+    private readonly IRepositoryBase<Domain.Entities.OrganizationInfo, Guid> _organizationInfoRepository;
+    private readonly IRepositoryBase<Domain.Entities.Category, Guid> _categoryRepository;
+    private readonly IUnitOfWork _unitOfWork;
+
+    public CreateEventCommandHandler(
+        IRepositoryBase<Domain.Entities.Event, Guid> eventRepository,
+        IRepositoryBase<Domain.Entities.OrganizationInfo, Guid> organizationInfoRepository,
+        IRepositoryBase<Domain.Entities.Category, Guid> categoryRepository,
+        IUnitOfWork untiOfWork)
+    {
+        _eventRepository = eventRepository;
+        _unitOfWork = untiOfWork;
+        _organizationInfoRepository = organizationInfoRepository;
+        _categoryRepository = categoryRepository;
+    }
+
+    public async Task<Result> Handle(Command.CreateEventCommand request, CancellationToken cancellationToken)
+    {
+        // Step 01: check organization existing?
+        var holderOrganizationInfo = await _organizationInfoRepository.FindByIdAsync(request.OrganizationId, cancellationToken)
+            ?? throw new OrganizationInfoException.OrganizationNotFoundException(request.OrganizationId);
+
+        // Step 02: check category existing?
+        if (request.CategoryId.HasValue)
+        {
+            var category = await _categoryRepository.FindByIdAsync(request.CategoryId.Value, cancellationToken)
+                ?? throw new OrganizationInfoException.OrganizationNotFoundException(request.CategoryId.Value);
+        }
+
+        // Step 03: Create event
+        var @event = Domain.Entities.Event.Create(
+            request.Name,
+            request.Description,
+            request.StartedDateOnUtc,
+            request.EndedDateOnUtc,
+            request.Capacity,
+            request.CategoryId,
+            holderOrganizationInfo.Id,
+            request.MeetUrl,
+            request.Adrress,
+            request.District,
+            request.City,
+            request.Country,
+            request.EventType);
+
+        
+        // Peristence into DB
+        _eventRepository.Add(@event);
+        await _unitOfWork.SaveChangesAsync(cancellationToken);
+
+        return Result.Success();
+    }
+}
